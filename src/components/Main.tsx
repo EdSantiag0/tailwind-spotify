aed0db4447fac86f2420c2c3f61f006f22f01176
Eduardo Santiago <eduardosantiago_@hotmail.com>
2024-01-26 10:23:21 -0300

    feat: added new images

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export function Main() {
   return (
-    <main className="flex-1 p-6 bg-gradient-to-b from-green-900 via-zinc-950 to-zinc-950">
+    <main className="flex-1 p-4 bg-gradient-to-b from-green-900 via-zinc-950 to-zinc-950">
       <div className="flex items-center gap-4">
         <button className="rounded-full bg-black/40 p-1">
           <ChevronLeft />
@@ -23,12 +23,12 @@ export function Main() {
           className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/wasting-light.jpg"
             width={106}
             height={106}
             alt="Capa de álbum"
           />
-          <strong>Wasting Ligth</strong>
+          <strong>Wasting Light</strong>
 
           <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
             <Play style={{ fill: "black" }} />
@@ -39,12 +39,12 @@ export function Main() {
           className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/album-musica-para-meditar.jpg"
             width={106}
             height={106}
             alt="Capa de álbum"
           />
-          <strong>Wasting Ligth</strong>
+          <strong>Musica para Meditação</strong>
           <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
             <Play style={{ fill: "black" }} />
           </button>
@@ -54,12 +54,12 @@ export function Main() {
           className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/album-rock-collection.jpg"
             width={106}
             height={106}
             alt="Capa de álbum"
           />
-          <strong>Wasting Ligth</strong>
+          <strong>Rock Collection</strong>
           <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
             <Play style={{ fill: "black" }} />
           </button>
@@ -69,12 +69,12 @@ export function Main() {
           className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/album-jazz-mix.jpeg"
             width={106}
             height={106}
             alt="Capa de álbum"
           />
-          <strong>Wasting Ligth</strong>
+          <strong>Jazz Mix</strong>
           <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
             <Play style={{ fill: "black" }} />
           </button>
@@ -84,12 +84,12 @@ export function Main() {
           className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/album-night-visions.jpeg"
             width={106}
             height={106}
             alt="Capa de álbum"
           />
-          <strong>Wasting Ligth</strong>
+          <strong>Night Visions</strong>
           <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
             <Play style={{ fill: "black" }} />
           </button>
@@ -99,12 +99,12 @@ export function Main() {
           className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/matando-robos-gigantes.jpg"
             width={106}
             height={106}
             alt="Capa de álbum"
           />
-          <strong>Wasting Ligth</strong>
+          <strong>Matando Robôs Gigantes</strong>
           <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
             <Play style={{ fill: "black" }} />
           </button>
@@ -122,15 +122,15 @@ export function Main() {
           className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/album-rock-mix.jpeg"
             className="w-full"
             width={120}
             height={120}
             alt="Capa de álbum"
           />
-          <strong className="font-semibold">Daily Mix 1</strong>
+          <strong className="font-semibold">Rock Mix</strong>
           <span className="text-sm text-zinc-400">
-            Wallows, COIN, girl in red and more
+            AC/DC, Metallica, Guns and Roses, Led Zeppelin
           </span>
         </a>
         <a
@@ -138,15 +138,15 @@ export function Main() {
           className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/mix-trip.jpg"
             className="w-full"
             width={120}
             height={120}
             alt="Capa de álbum"
           />
-          <strong className="font-semibold">Daily Mix 1</strong>
+          <strong className="font-semibold">Trip Mix Vol3</strong>
           <span className="text-sm text-zinc-400">
-            Wallows, COIN, girl in red and more
+            Oasis, Eagles, The Doors, Audioslave
           </span>
         </a>
         <a
@@ -154,15 +154,15 @@ export function Main() {
           className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/album-jazz-mix.jpeg"
             className="w-full"
             width={120}
             height={120}
             alt="Capa de álbum"
           />
-          <strong className="font-semibold">Daily Mix 1</strong>
+          <strong className="font-semibold">Jazz Mix</strong>
           <span className="text-sm text-zinc-400">
-            Wallows, COIN, girl in red and more
+            Nina Simone, Chet Backer, Gregory Porter
           </span>
         </a>
         <a
@@ -170,15 +170,15 @@ export function Main() {
           className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/mix-summer.jpeg"
             className="w-full"
             width={120}
             height={120}
             alt="Capa de álbum"
           />
-          <strong className="font-semibold">Daily Mix 1</strong>
+          <strong className="font-semibold">Summer Mix Vol1</strong>
           <span className="text-sm text-zinc-400">
-            Wallows, COIN, girl in red and more
+            Morron 5, Seu Jorge, Natirus, Chimarruts
           </span>
         </a>
         <a
@@ -186,15 +186,15 @@ export function Main() {
           className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
         >
           <Image
-            src="/album-wasting-light.jpg"
+            src="/mix-retro.jpg"
             className="w-full"
             width={120}
             height={120}
             alt="Capa de álbum"
           />
-          <strong className="font-semibold">Daily Mix 1</strong>
+          <strong className="font-semibold">Retro Mix</strong>
           <span className="text-sm text-zinc-400">
-            Wallows, COIN, girl in red and more
+            Bryan Ferry, Bonnie Tyler, Chicago, The Housemartins
           </span>
         </a>
       </div>
